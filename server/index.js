bcfe2cffa1eae003ec9c3c0800d124faacd82184
shubham shankar <msgtoshubham@gmail.com>
2023-07-30 20:26:35 +0530

    implements group addition and display

--- server/index.js
+++ server/index.js
@@ -1,23 +1,44 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+var bodyParser = require('body-parser')
 var mongoose = require('mongoose')
+
+
 
 var Message = mongoose.model('MessageType' , {
   content:String,
   sender:String,
   senderName:String,
-  date:Date
+  date:Date,
+  messageId:String
+});
+
+var Group = mongoose.model('GroupType' , {
+  groupId : String
 });
 
 
 var dbUrl = "mongodb+srv://shubham28:shubham28@cluster0.uo1zeww.mongodb.net/?retryWrites=true&w=majority";
 
 const app = express();
+
+app.use(express.json())
+// app.use(bodyParser.urlencoded({extended:false}))
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const httpServer = createServer(app);
 const io = new Server(httpServer, { cors : {
     origin:["http://localhost:3000","http://localhost:3006","http://localhost:3007" ]
 } });
+
+
+// app.use(express.urlencoded({ extended: true }));
 
 io.use((socket, next) => {
   const username = socket.handshake.auth.username;
@@ -42,10 +63,12 @@ io.on("connection", (socket) => {
       username: socket.username,
     });
   }
+
   socket.emit("retrieve userlist",users);
 
   //emit a chat message to all users
   socket.on('chat message' , (msg) => {
+    msg['messageId'] = null;
     let newMessage = new Message(msg);
   
     newMessage.save()
@@ -79,6 +102,34 @@ io.on("connection", (socket) => {
 mongoose.connect(dbUrl).catch((err) => {
   console.log("catching the error ......")
   console.log(err)
+});
+
+
+app.get('/groups' , (request,response) => {
+  Group.find({})
+  .catch((err) => {
+    console.log(err);
+  })
+  .then((groups) => {
+    console.log(groups);
+    response.send(groups);
+  })
+
 })
+
+app.post('/groups' , (req, res) => {
+    let newGroup = new Group(req.body);
+    newGroup.save()
+    .then(() => {
+
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+    
+    console.log(req.body);
+})
+
+
 
 httpServer.listen(8000);
