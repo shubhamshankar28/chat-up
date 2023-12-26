d4d7ee6ce0b0238828c7fb9351ad5b43f26d1eac
shubham shankar <msgtoshubham@gmail.com>
2023-12-26 23:58:59 +0530

    Retrieve PORT and database URL from environment variables

--- server/index.js
+++ server/index.js
@@ -2,19 +2,20 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import mongoose from "mongoose"
-import {defineAuthenticationEndpoints} from './authenticationEndpoints.js';
-import {defineChatEndpoints} from './chatEndpoints.js';
-import {defineMembershipEndpoints} from './membershipEndpoints.js';
-import {defineSocketLogic} from './socketLogic.js';
+import { defineAuthenticationEndpoints } from './authenticationEndpoints.js';
+import { defineChatEndpoints } from './chatEndpoints.js';
+import { defineMembershipEndpoints } from './membershipEndpoints.js';
+import { defineSocketLogic } from './socketLogic.js';
 import dbUrl from './secretKey.js';
 
-var databaseUrl = dbUrl;
+const PORT = process.env.PORT || 8000
+var databaseUrl = process.env.MONGODB_CONNECT_URI;
 const app = express();
 
 app.use(express.json())
 // app.use(bodyParser.urlencoded({extended:false}))
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -22,15 +23,17 @@ app.use(function(req, res, next) {
 
 const httpServer = createServer(app);
 
-const io = new Server(httpServer, { cors : {
-    origin:["http://localhost:3000","http://localhost:3006","http://localhost:3007" ]
-} });
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["http://localhost:3000", "http://localhost:3006", "http://localhost:3007"]
+  }
+});
 
 
 defineSocketLogic(io);
 defineAuthenticationEndpoints(app);
-defineChatEndpoints(io,app);
-defineMembershipEndpoints(io,app);
+defineChatEndpoints(io, app);
+defineMembershipEndpoints(io, app);
 
 mongoose.connect(databaseUrl).catch((err) => {
   console.log("catching the error ......")
@@ -38,7 +41,7 @@ mongoose.connect(databaseUrl).catch((err) => {
 });
 
 
-httpServer.listen(8000, () => {
+httpServer.listen(PORT, () => {
 
 });
 
